509e8f33eb37958eddd14daf9b756faeea62c8a8
Julia Julina <julia.jjv@gmail.com>
2019-10-01 18:25:03 +0300

    feat: implement function decode

--- src/index.js
+++ src/index.js
@@ -39,7 +39,65 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let words = expr.split('**********');
+    let decode_word = function(word_digital) {
+        let word_human = '';
+        let start = 0;
+        let end = word_digital.length;
+
+        let get_letter = function () {
+            let letter_digital = word_digital.substr(start, 10);
+
+            function to_morse() {
+                let letter_morse = '';
+                let s = 0;
+
+                function get_symb () {
+                    let symb = letter_digital.substr(s, 2);
+                    return {
+                        convert: function() {
+                            if (symb === '10') {
+                                return '.';
+                            } 
+                            else if (symb === '11') {
+                                return '-';
+                            } 
+                            return '';
+                        }
+                    }
+                }
+                
+                while (s < 10) {
+                    let symb = get_symb();
+                    letter_morse += symb.convert();
+                    s = s + 2;
+                }
+
+                return letter_morse;
+            }
+            return {
+                to_human: function() {
+                    let letter = to_morse();
+                    return MORSE_TABLE[letter];
+                }
+            }
+        }
+
+        while (start < end) {
+            let letter = get_letter();
+            word_human += letter.to_human();
+            start += 10;
+        }
+        return word_human;
+    }  
+
+    let words_human = words.map(function(current) {
+        return decode_word(current);
+    }); 
+      
+    return words_human.join(' ');
 }
+
 
 module.exports = {
     decode
